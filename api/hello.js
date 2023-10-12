d88ea29cc1823c73cfd2932843f6f10aac87bcb2
heyuanyou <harrell16805@163.com>
2023-10-12 09:33:57 +0800

    fix api

--- api/hello.js
+++ api/hello.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
 
-app.get('/hello', (req, res) => {
-    console.log("hello request...");
+app.get('/api/hello', (req, res) => {
     res.json({message: 'Hello, World!'});
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+module.exports = app;
